56c56a5e6d936be3ad131fb1381b3112eb6df616
Rebecca Qian <RebeccaQian@Rebeccas-MacBook-Pro-2.local>
2016-08-09 22:33:37 -0500

    javascript functions

--- JS/resource_selection.js
+++ JS/resource_selection.js
@@ -14,12 +14,27 @@ function Resource(name, Url, difficulty) {
 }
 
 // full list of resources
-var resources = [ new Resource('Codeacademy', 'https://www.codecademy.com/', 2),
-new Resource('Coursera', 'https://www.coursera.com', 3), 
+var resources = [ new Resource('Codeacademy', 'https://www.codecademy.com/', 3),
+new Resource('Coursera', 'https://www.coursera.com', 4), 
 new Resource('Khanacademy', 'https://khanacademy.com', 2)];
 
 // function takes in user selections
 // pushes to arrays of links
+function sortLevel (level) {
+	for (var i = 0; i < resources.length; i++) {
+		if (level == 'middleSchool' && resources[i].level == 2) {
+			result.push(resources[i]);
+		}
+		if (level == 'highSchool' && resources[i].level == 3) {
+			result.push(resources[i]);
+		}
+		if (level == 'college' && resources[i].level == 4) {
+			result.push(resources[i]);
+		}
+	}
+	return result;
+}
+
 // function resourceList(age, subject, goal, style){
 
 // }
