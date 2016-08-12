1d6e8d7c9b28ca438bdbdbc21868432c563f640e
Rebecca Qian <RebeccaQian@Rebeccas-MacBook-Pro-2.local>
2016-08-12 18:26:30 -0500

    fixed repeated inputs for subject and education

--- JS/resource_selection.js
+++ JS/resource_selection.js
@@ -10,16 +10,19 @@ var interestList = [];
 var categories = [];
 
 // Resource object constructor
-function Resource(name, Url, difficulty) {
+function Resource(name, Url, difficulty, learningStyle) {
 	this.name = name;
 	this.Url = Url;
 	this.level = difficulty;
+	this.learningStyle = learningStyle;
 }
 
 // full list of resources
-var resources = [ new Resource('Codeacademy', 'https://www.codecademy.com/', 3),
-new Resource('Coursera', 'https://www.coursera.com', 4), 
-new Resource('Khanacademy', 'https://khanacademy.com', 2)];
+var resources = [ new Resource('Codeacademy', 'https://www.codecademy.com/', 3, ['Reading']),
+new Resource('Coursera', 'https://www.coursera.com', 4, ['Video', 'Friends']),
+new Resource('VideoResource', 'https://www.coursera.com', 3, ['Video', 'Friends']),
+new Resource('RandomResource', 'https://www.video.com', 3, ['Video', 'Friends']), 
+new Resource('Khanacademy', 'https://khanacademy.com', 2, ['Video', 'Games'])];
 
 // Company object constructor
 function Company(name, Url, category, summary) {
@@ -37,26 +40,80 @@ new Company('Spotify', 'www.spotify.com', 'Music', 'hosts a large music library 
 new Company('Doordash', 'www.doordash.com', 'Food', 'delivers food to consumers from a wide range of restaurants'),
 new Company('Pinterest', 'www.pinterest.com', 'Art', 'shows pins ranging from fashion trends to photography')];
 
+function checkEqual (category, array) {
+	for (var k = 0; k < array.length; k++) {
+		if (category == array[k]) {
+			return false;
+		}
+	}
+	return true;
+}
+
 (function getInterest() {
 	function sortArt() {
-		interestList.push('Art');
+		if (checkEqual('Art', interestList)) {
+			interestList.push('Art');
+		}
 	}
 	function sortSports() {
-		interestList.push('Sports');
+		if (checkEqual('Sports', interestList)) {
+			interestList.push('Sports');
+		}
 	}
 	function sortTravel() {
-		interestList.push('Travel');
+		if (checkEqual('Travel', interestList)) {
+			interestList.push('Travel');
+		}
+	}
+	function sortScience() {
+		if (checkEqual('Science', interestList)) {
+			interestList.push('Science');
+		}
+	}
+	function sortMusic() {
+		if (checkEqual('Music', interestList)) {
+			interestList.push('Music');
+		}
+	}
+	function sortMath() {
+		if (checkEqual('Math', interestList)) {
+			interestList.push('Math');
+		}
+	}
+	function sortGames() {
+		if (checkEqual('Games', interestList)) {
+			interestList.push('Games');
+		}
+	}
+	function sortFashion() {
+		if (checkEqual('Fashion', interestList)) {
+			interestList.push('Fashion');
+		}
+	}
+	function sortFood() {
+		if (checkEqual('Food', interestList)) {
+			interestList.push('Food');
+		}
 	}
 
-	 document.getElementById('chooseArt').addEventListener("click", sortArt);
+	document.getElementById('chooseArt').addEventListener("click", sortArt);
 	document.getElementById('chooseSports').addEventListener("click", sortSports);
-	 document.getElementById('chooseTravel').addEventListener("click", sortTravel);
+	document.getElementById('chooseTravel').addEventListener("click", sortTravel);
+	document.getElementById('chooseScience').addEventListener("click", sortScience);
+	document.getElementById('chooseMusic').addEventListener("click", sortMusic);
+	document.getElementById('chooseMath').addEventListener("click", sortMath);
+	document.getElementById('chooseGames').addEventListener("click", sortGames);
+	document.getElementById('chooseFashion').addEventListener("click", sortFashion);
+	document.getElementById('chooseFood').addEventListener("click", sortFood);
 
 	//array of user interests
 	return interestList;
 })();
 
 // sorts user interest
+// categories input is array of user selected interests eg. Art, Science etc.
+// iterates through list of companies, pushing to companyList array if there's a match
+// for companies with multiple categories, checks that company isn't already in companyList array
 function sortInterest (categories) {
 	for (var j = 0; j < categories.length; j++) {
 		for (var i = 0; i < companies.length; i++) {
@@ -77,7 +134,7 @@ function sortInterest (categories) {
 
 	  	for (var i = 0; i < companyList.length; i++) {
 	 	
-	 	document.getElementById("grandResults").innerHTML += companyList[i].name + ": " + companyList[i].summary + "<br />";
+	 	document.getElementById("grandResults2").innerHTML += companyList[i].name + ": " + companyList[i].summary + "<br />";
 	 }
 	}
 document.getElementById("finalPrompt").addEventListener("click", sortInterests);
@@ -87,14 +144,17 @@ document.getElementById("finalPrompt").addEventListener("click", sortInterests);
 // function takes in user selections
 // pushes to arrays of links
 function sortLevel (level) {
+	result = [];
 	for (var i = 0; i < resources.length; i++) {
 		if (level == 'middleSchool' && resources[i].level == 2) {
+			//result = [];
 			result.push(resources[i]);
 		}
 		if (level == 'highSchool' && resources[i].level == 3) {
 			result.push(resources[i]);
 		}
 		if (level == 'college' && resources[i].level == 4) {
+			//result = [];
 			result.push(resources[i]);
 		}
 	}
@@ -102,27 +162,70 @@ function sortLevel (level) {
 }
 
 (function getLevel() {
+	result = [];
 	function sortMS() {
 		return sortLevel('middleSchool');
 	}
 	function sortHS() {
+		//result = [];
 		return sortLevel('highSchool');
 	}
 	function sortCollege() {
+		//result = [];
 		return sortLevel('college');
 	}
 	document.getElementById("chooseMS").addEventListener("click", sortMS);
 	document.getElementById("chooseHS").addEventListener("click", sortHS);
 	document.getElementById("chooseCollege").addEventListener("click", sortCollege);
 
+	//result = [];
+
 })();
 
 (function showResults() {
 	function sortResults() {
+		document.getElementById("grandResults1").innerHTML = "";
 		for (var i = 0; i < result.length; i++) {
-			document.getElementById("grandResults").innerHTML += result[i].name + ": " + result[i].Url + " <br />";
+			document.getElementById("grandResults1").innerHTML += result[i].name + ": " + result[i].Url + " <br />";
 		}
 	}
 
 	document.getElementById("finalPrompt").addEventListener("click", sortResults);
+	result = [];
 })();
+
+// (function getLearningStyle() {
+// 	function sortVideo() {
+// 		for (var i = 0; i < result.length; i++) {
+// 			for (var j = 0; j < result[i].learningStyle.length; j++) {
+// 				if (result[i].learningStyle[j] != 'Video') {
+// 					result[i] = '';
+// 				}
+// 			}
+// 		}
+// 		return result;
+// 	}
+// 	function sortReading() {
+// 		return;
+// 	}
+// 	function sortFriends() {
+// 		return;
+// 	}
+// 	function sortGames() {
+// 		return;
+// 	}
+// 	document.getElementById("chooseVideo").addEventListener("click", sortVideo);
+// 	document.getElementById("chooseReading").addEventListener("click", sortReading);
+// 	document.getElementById("chooseFriends").addEventListener("click", sortFriends);
+// 	document.getElementById("chooseGames").addEventListener("click", sortGames);
+// })
+
+// function testfn() {
+// 	return interestList.push('Travel');
+// }
+
+// document.getElementById("chooseTravel").addEventListener("click", testfn);
+
+// function resourceList(age, subject, goal, style){
+
+// }
